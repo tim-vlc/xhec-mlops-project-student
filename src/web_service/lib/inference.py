c7f44ae077b425cde8dd4dc9fe2c8de77efebd9b
Tim <timvalencony@gmail.com>
2024-10-24 17:43:37 +0200

    feat(save encoder)

--- src/web_service/lib/inference.py
+++ src/web_service/lib/inference.py
@@ -4,15 +4,14 @@ from typing import List
 import pandas as pd
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import OneHotEncoder
-from app_config import CATEGORICAL_COLS, NUMERICAL_COLS, TARGET_COLS
+from app_config import CATEGORICAL_COLS, NUMERICAL_COLS
 from loguru import logger
 import numpy as np
 
-from models import InputData
+from lib.models import InputData
 
-def preprocess_X(df: pd.DataFrame):
-    encoder = OneHotEncoder(drop="first", sparse_output=False)
-    encoded_categorical = encoder.fit_transform(df[CATEGORICAL_COLS])
+def preprocess_X(df: pd.DataFrame, encoder: OneHotEncoder):
+    encoded_categorical = encoder.transform(df[CATEGORICAL_COLS].astype('category'))
     encoded_df = pd.DataFrame(
         encoded_categorical, columns=encoder.get_feature_names_out(CATEGORICAL_COLS)
     )
@@ -21,19 +20,18 @@ def preprocess_X(df: pd.DataFrame):
 
     return X
 
-def run_inference(input_data: List[InputData], model: BaseEstimator) -> np.ndarray:
-    """Run inference on a list of input data.
-    Args:
-        payload (dict): the data point to run inference on.
-        model (BaseEstimator): the fitted model object.
-    Returns:
-        np.ndarray: the predicted trip durations in minutes.
-    Example payload:
-        {'PULocationID': 264, 'DOLocationID': 264, 'passenger_count': 1}
-    """
+def run_inference(input_data: List[InputData], model: BaseEstimator, encoder: OneHotEncoder) -> np.ndarray:
     logger.info(f"Running inference on:\n{input_data}")
-    df = pd.DataFrame([x.dict() for x in input_data])
-    X = preprocess_X(df)
+
+    print(input_data[0].model_dump())
+
+    df = pd.DataFrame([
+        {key.replace("_", " "):value for key, value in x.model_dump().items()}
+        for x in input_data
+    ],
+    columns = NUMERICAL_COLS + CATEGORICAL_COLS)
+
+    X = preprocess_X(df, encoder)
 
     y = model.predict(X)
     logger.info(f"Predicted trip durations:\n{y}")
