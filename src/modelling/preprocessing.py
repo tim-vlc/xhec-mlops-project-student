3dabe4535f0c16c988fa5ce7ec6c60848b539435
malo-renaudin <malo.renaudin@hec.edu>
2024-10-24 14:03:57 +0200

    fix(update preprocessing)

---
+++ src/modelling/preprocessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+
+def preprocess_data(df, numerical_cols, categorical_cols, target_col):
+
+    encoder = OneHotEncoder(drop='first', sparse_output=False)
+    encoded_categorical = encoder.fit_transform(df[categorical_cols])
+    encoded_df = pd.DataFrame(
+        encoded_categorical, columns=encoder.get_feature_names_out(categorical_cols))
+
+    X = pd.concat([df[numerical_cols], encoded_df], axis=1)
+    y = df[target_col]
+
+    return X, y
